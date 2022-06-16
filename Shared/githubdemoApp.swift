f098e36033624a5b8678ae7015cf3d1b8393a5ed
Hyeon-Jun-Kim <gomji81@naver.com>
2022-06-16 16:30:07 +0900

    Initial Commit

---
+++ Shared/githubdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubdemoApp.swift
+//  Shared
+//
+//  Created by 김현준 on 2022/06/16.
+//
+
+import SwiftUI
+
+@main
+struct githubdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
